dfc9650d9e2cb72472895bf97315868644e9f070
Get-Tony <Get-Tony@outlook.com>
2023-01-24 12:20:17 +0100

    added service skeleton

--- restless/services.py
+++ restless/services.py
@@ -1,2 +1,17 @@
 """Main module for Restless."""
 __author__ = "Anthony Pagan <get-tony@outlook.com>"
+
+
+class Service:
+    """Base class for all services."""
+
+    def __init__(self):
+        """Initialize the service."""
+
+    def __call__(self, *args, **kwargs):
+        """Call the service."""
+        return self.run(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        """Run the service."""
+        raise NotImplementedError("Service.run() must be implemented.")
